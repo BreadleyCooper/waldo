fb38238c8e668e2211ec4a7c9123cbada8cdcb42
Jordan Ulmer <julmer526@gmail.com>
2023-03-24 11:29:37 +0000

    cleaned up - firebase not working though

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,8 +1,6 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+const config = {
   apiKey: "AIzaSyBARCvyTHTfzgGGOfQi6jwjqA1ZZ2IdrWA",
   authDomain: "where-waldo-90f28.firebaseapp.com",
   projectId: "where-waldo-90f28",
@@ -11,10 +9,21 @@ const firebaseConfig = {
   appId: "1:738887008121:web:ea82ed4d1155c9366a39a8"
 };
 
-firebase.initializeApp(firebaseConfig);
+export function getFirebaseConfig() {
+  if (!config || !config.apiKey) {
+    throw new Error('No Firebase configuration object provided.' + '\n' +
+    'Add your web app\'s configuration object to firebase-config.js');
+  } else {
+    return config;
+  }
+}
 
-const firestore = firebase.firestore();
 
-const timestamp = firebase.firestore.FieldValue.serverTimestamp();
-
-export { firestore, timestamp };
+const firebaseConfig = {
+  apiKey: "AIzaSyBARCvyTHTfzgGGOfQi6jwjqA1ZZ2IdrWA",
+  authDomain: "where-waldo-90f28.firebaseapp.com",
+  projectId: "where-waldo-90f28",
+  storageBucket: "where-waldo-90f28.appspot.com",
+  messagingSenderId: "738887008121",
+  appId: "1:738887008121:web:ea82ed4d1155c9366a39a8"
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { initializeApp } from 'firebase/app';
+import { getFirebaseConfig } from "./firebase-config.js"
+
+
+const firebaseAppConfig = getFirebaseConfig()
+
+initializeApp(firebaseAppConfig)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +18,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
