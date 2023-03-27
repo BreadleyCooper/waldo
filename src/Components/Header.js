7e17d8397b98f82b7d2176474ce3d805625bfb13
Jordan Ulmer <julmer526@gmail.com>
2023-03-27 17:33:38 +0100

    added image and initial components

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = (props) => {
+
+    return (
+        <div className="headerContainer">
+            <h1>Where's Skywaldoker?</h1>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/Components/Main.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Main = (props) => {
+
+    return (
+        <div className="mainConatinre">
+            <img className="mainImage" alt="Star Wars Characters" src="images/waldoImage.jpg"></img>
+        </div>
+    )
+}
+
+export default Main
